851306359c77a2381632a6389f63f1ed1081ec9f
KERRBR <brandon.kerr@lhsc.on.ca>
2022-02-11 11:17:45 -0500

    Admin page to view users and their check lists

--- resources/views/checklist/index.blade.php
+++ resources/views/checklist/index.blade.php
@@ -2,6 +2,9 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ $all ?  __('checklist.all') :  __('checklist.completed')}}
+            @if (!$user->is(Auth::user()))
+                for {{ $user->name }}
+            @endif
         </h2>
     </x-slot>
 
