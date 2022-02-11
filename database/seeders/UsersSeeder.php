18506e90aeb4a7a1becb98477138b5cb47898df8
KERRBR <brandon.kerr@lhsc.on.ca>
2022-02-11 11:20:52 -0500

    Add seeders

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Awesome Admin',
+            'email' => 'admin@todo.com',
+            'password' => Hash::make('admin'),
+            'email_verified_at' => now(),
+            'is_admin' => true
+        ]);
+
+        $this->command->info('Admin user created');
+
+        User::create([
+            'name' => 'Normal User',
+            'email' => 'user@todo.com',
+            'password' => Hash::make('user'),
+            'email_verified_at' => now()
+        ]);
+
+        $this->command->info('Standard user created');
+    }
+}
